93b95befb0965ea791bdd46a383f29c013c0fc63
arch-fan <juanserracines@gmail.com>
2024-04-06 20:28:07 +0200

    site to base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-const { DEV, SITE } = import.meta.env;
+const { DEV, BASE_URL } = import.meta.env;
 
 export const redirectUri = DEV
   ? "http://localhost:4321/spotify-stats/callback"
-  : `${SITE}/spotify-stats/callback`;
+  : `${BASE_URL}/callback`;
