b79d857ecc50227019998bc74eea9c42f68db7a5
arch-fan <juanserracines@gmail.com>
2024-04-07 02:56:02 +0200

    Responsive artists

--- src/components/stats/Artists.svelte
+++ src/components/stats/Artists.svelte
@@ -4,7 +4,7 @@
 </script>
 
 {#if store.artistsData}
-  <div class="grid grid-cols-4 gap-1" transition:fade>
+  <div class="grid lg:grid-cols-4 grid-cols-3 gap-1" transition:fade>
     {#each store.artistsData.items as artist, i}
       <div class="flex flex-col p-1 gap-2 rounded items-center relative">
         <span class="absolute text-5xl opacity-50 font-extrabold top-0 left-1"
@@ -15,8 +15,12 @@
           alt={artist.name}
           class="h-24 w-24 rounded-full"
         />
-        <h2 class="font-bold">{artist.name}</h2>
-        <p>{artist.followers.total} followers</p>
+        <h2 class="font-bold text-center" style="overflow-wrap: anywhere;">
+          {artist.name}
+        </h2>
+        <!-- <p class="line-clamp-1 mt-auto">
+          {artist.followers.total}
+        </p> -->
       </div>
     {/each}
   </div>
