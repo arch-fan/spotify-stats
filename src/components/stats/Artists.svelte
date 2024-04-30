a3f6a9e337d6f67d296eaff78fef0b57beb5bd7a
arch-fan <juanserracines@gmail.com>
2024-05-01 00:13:30 +0200

    More opacity to font

--- src/components/stats/Artists.svelte
+++ src/components/stats/Artists.svelte
@@ -7,7 +7,8 @@
   <div class="grid lg:grid-cols-4 grid-cols-3 gap-1" transition:fade>
     {#each store.artistsData.items as artist, i}
       <div class="flex flex-col p-1 gap-2 rounded items-center relative">
-        <span class="absolute text-5xl opacity-50 font-extrabold top-0 left-1"
+        <span
+          class="absolute text-5xl bg-gradient-to-br from-white/50 to-white text-transparent bg-clip-text font-extrabold top-0 left-1"
           >{i + 1}</span
         >
         <img
